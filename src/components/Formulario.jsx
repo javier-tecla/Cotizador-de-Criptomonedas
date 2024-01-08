c5493123bad14e6bd54870144c693bfafc4bc134
javierborjas <cristman11@gmail.com>
2024-01-07 23:48:29 -0300

    Utilizando nuestro Hook y pasando opciones al Hook

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import styled from '@emotion/styled'
 import useSelectMonedas from '../hooks/useSelectMonedas'
+import { monedas } from '../data/monedas'
 
 const InputSubmit = styled.input`
     background-color: #9497FF;
@@ -23,17 +24,23 @@ const InputSubmit = styled.input`
 
 const Formulario = () => {
 
-  const [ SelectMonedas ] = useSelectMonedas()
-  SelectMonedas()
 
-  return (
-        <form>
-      
-            <InputSubmit 
-                    type='submit' 
-                    value='Cotizar' />
-        </form>
-  )
-}
+
+    const [ SelectMonedas ] = useSelectMonedas('Elige tu Moneda', monedas)
+    
+  
+        return (
+                <form>
+
+                <SelectMonedas />
+                
+            
+                    <InputSubmit 
+                            type='submit' 
+                            value='Cotizar' 
+                    />
+                </form>
+        )
+    }
 
 export default Formulario
--- src/hooks/useSelectMonedas.jsx
+++ src/hooks/useSelectMonedas.jsx
@@ -1,11 +1,28 @@
 import React from 'react'
+import styled from '@emotion/styled'
 
-const useSelectMonedas = () => {
+const Label = styled.label`
+    color: #FFFFFF;
+
+`
+
+const useSelectMonedas = (label, opciones) => {
   
+    const SelectMonedas = () => (
+        <>
+            <Label>{label}</Label>
+            <select>
+                <option value="">Seleccione</option>
 
-    const SelectMonedas = () => {
-        console.log('Desde Select Monedas')
-    }
+                {opciones.map( opcion => (
+                   <option 
+                        key={opcion.id}
+                        value={opcion.id}
+                   >{opcion.nombre}</option> 
+                ))}
+            </select>
+        </>
+    )
 
     return [ SelectMonedas ]
 }
