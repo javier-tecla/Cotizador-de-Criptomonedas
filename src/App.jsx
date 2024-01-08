cb3b025a91f372f41bbc12101bd6e3de1981535e
javierborjas <cristman11@gmail.com>
2024-01-08 20:15:51 -0300

    Añadiendo un Spinner

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState,  useEffect } from 'react'
 import styled from '@emotion/styled'
 import Formulario from './components/Formulario'
 import Resultado from './components/Resultado'
+import Spinner from './components/Spinner'
 import ImagenCripto from './img/imagen-criptos.png'
 
 const Contenedor = styled.div`
@@ -44,11 +45,15 @@ function App() {
 
   const [ monedas, setMonedas ] = useState({})
   const [ resultado, setResultado ] = useState({})
+  const [ cargando, setCargando ] = useState(false)
 
   useEffect(() => {
     if(Object.keys(monedas).length > 0) {
 
         const cotizarCripto = async () => {
+          setCargando(true)
+          setResultado({})
+
             const { moneda, criptomoneda } = monedas
             const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomoneda}&tsyms=${moneda}`
 
@@ -56,6 +61,8 @@ function App() {
             const resultado = await respuesta.json()
 
             setResultado(resultado.DISPLAY[criptomoneda][moneda])
+
+            setCargando(false)
         }
 
         cotizarCripto();
@@ -74,7 +81,7 @@ function App() {
             <Formulario 
               setMonedas={setMonedas}
             />
-
+            {cargando && <Spinner />}
             {resultado.PRICE && <Resultado resultado={resultado} />}
         </div>
 
@@ -83,3 +90,4 @@ function App() {
 }
 
 export default App
+
