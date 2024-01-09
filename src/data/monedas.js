bff3c7256897070888b3c81013cb7743b8a2c2f0
javierborjas <cristman11@gmail.com>
2024-01-08 21:08:09 -0300

    Añadiendo mas monedas de paises y colocando className a Spinner.jsx

--- src/data/monedas.js
+++ src/data/monedas.js
@@ -1,8 +1,14 @@
 const monedas = [
-    { id: 'USD', nombre: 'Dolar de Estados Unidos'},
-    { id: 'EUR', nombre: 'Euro'},
-    { id: 'ARS', nombre: 'Peso Argentino'},
-    { id: 'GBP', nombre: 'Libra Esterlina'},
+    { id: 'USD', nombre: 'Dólar americano - USD'},
+    { id: 'EUR', nombre: 'Euro - EUR'},
+    { id: 'CAD', nombre: 'Dólar canadiense - CAD'},
+    { id: 'GBP', nombre: 'Libra esterlina - GBP'},
+    { id: 'CHF', nombre: 'Franco suizo - CHF'},
+    { id: 'JPY', nombre: 'Yen japonés - JPY'},
+    { id: 'MXN', nombre: 'Peso mexicano - MXN'},
+    { id: 'COP', nombre: 'Peso colombiano - COP'},
+    { id: 'BRL', nombre: 'Real brasileño - BRL'},
+    { id: 'ARS', nombre: 'Peso Argentino - ARS'},
 ]
 
 export {
